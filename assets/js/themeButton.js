7fbabaf066a9b670be17fcab9cdc52de9ec014dd
Qinzi Zhang <zqz20100221@126.com>
2024-07-24 13:49:36 -0400

    store darkmode locally

--- assets/js/themeButton.js
+++ assets/js/themeButton.js
@@ -1,10 +1,16 @@
 function switchDarkMode() {
-    var wrapper = document.getElementsByClassName('theme')[0];
-    if (wrapper.classList.contains('theme-light')) {
-        wrapper.classList.remove('theme-light');
-        wrapper.classList.add('theme-dark');
-    } else {
-        wrapper.classList.remove('theme-dark');
-        wrapper.classList.add('theme-light');
-    }
+	// var wrapper = document.getElementsByClassName('theme')[0];
+	var wrapper = document.documentElement;
+	// var wrapper = document.body;
+	if (wrapper.classList.contains('theme-light')) {
+		// light to dark
+		wrapper.classList.remove('theme-light');
+		wrapper.classList.add('theme-dark');
+		localStorage.setItem('theme', 'theme-dark');
+	} else {
+		// dark to light
+		wrapper.classList.remove('theme-dark');
+		wrapper.classList.add('theme-light');
+		localStorage.setItem('theme', 'theme-light');
+	}
 }
